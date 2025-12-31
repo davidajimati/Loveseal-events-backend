18866d55ad20bc783337a2a14fa08f25cd8c54b3
David Ajimati <david.ajimati@stanbicibtc.com>
2025-12-31 11:39:36 +0100

    project initialized | dependencies added and started successfully

---
+++ dist/index.js
@@ -0,0 +1,44 @@
+import dns from "dns";
+import cors from "cors";
+import dotenv from "dotenv";
+import app from "./server.js";
+import bodyParser from "body-parser";
+import compression from 'compression';
+import express, {} from "express";
+dotenv.config();
+const corsOptions = {
+    origin: "*",
+    method: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+};
+const handleInvalidPayload = (err, req, res, next) => {
+    if (err instanceof SyntaxError && "body" in err) {
+        return res.status(400).json({
+            responseCode: "99",
+            responseMsg: "Invalid JSON payload",
+            responseData: err.message
+        });
+    }
+    next(err);
+};
+const handleInternalServerError = (err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({
+        responseCode: "100",
+        responseMsg: "Internal Server Error"
+    });
+    next(err);
+};
+app.use(compression());
+app.use(cors(corsOptions));
+app.use(handleInvalidPayload);
+app.use(handleInternalServerError);
+app.use(express.json({ limit: "2mb" }));
+app.options("*", cors(corsOptions));
+dns.setDefaultResultOrder("ipv4first");
+app.use(bodyParser.urlencoded({ extended: true }));
+const PORT = process.env.SERVER_PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import dns from "dns";
+import cors from "cors";
+import dotenv from "dotenv";
+import app from "./server.js"
+import bodyParser from "body-parser";
+import compression from 'compression';
+import {PrismaClient} from "@prisma/client"
+import express, {type ErrorRequestHandler} from "express";
+
+
+dotenv.config();
+const prisma = new PrismaClient({})
+const corsOptions = {
+    origin: "*",
+    method: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}
+
+const handleInvalidPayload: ErrorRequestHandler = (err, req, res, next) => {
+    if (err instanceof SyntaxError && "body" in err) {
+        return res.status(400).json({
+            responseCode: "99",
+            responseMsg: "Invalid JSON payload",
+            responseData: err.message
+        });
+    }
+    next(err);
+};
+
+const handleInternalServerError: ErrorRequestHandler = (err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({
+        responseCode: "100",
+        responseMsg: "Internal Server Error"
+    });
+    next(err);
+};
+
+async function connectDB() {
+    try {
+        await prisma.$connect();
+        console.log("✅ Connected to PostgreSQL via Prisma!");
+    } catch (err) {
+        console.error("❌ Failed to connect to PostgreSQL:", err);
+        process.exit(1);
+    }
+}
+
+connectDB().then(r => {
+    console.log("Prisma connection attempt completed")
+});
+
+app.use(compression());
+app.use(cors(corsOptions));
+app.use(handleInvalidPayload);
+app.use(handleInternalServerError);
+app.use(express.json({limit: "2mb"}));
+app.options("*", cors(corsOptions));
+dns.setDefaultResultOrder("ipv4first");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+const PORT = process.env.SERVER_PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
