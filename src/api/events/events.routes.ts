4372e505ce7d76fcc4780b11ac34f37891969edc
David Ajimati <david.ajimati@stanbicibtc.com>
2026-01-09 09:18:00 +0100

    prisma table created

--- src/api/events/events.routes.ts
+++ src/api/events/events.routes.ts
@@ -1,10 +1,14 @@
-import { Router } from "express";
+import {Router} from "express";
+import * as controller from "./events.controller.js"
 
 export default function registerEventRoutes(app: Router) {
     const router = Router();
 
-    router.get("/", (_req, res) => res.send("User list"));
-    router.post("/", (_req, res) => res.send("Create user"));
+    router.get("/", controller.getAllEvents);
+    router.get("/:id", controller.getEventById);
+    router.post("/", controller.createEvent);
+    router.put("/", controller.updateEvent);
+    router.delete("/:id", controller.deleteEvent);
 
     app.use("/events", router);
 }
