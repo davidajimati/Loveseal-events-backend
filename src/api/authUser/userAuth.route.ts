2bf35f8c121d53777850b8e5f7b33404c332239e
David Ajimati <david.ajimati@stanbicibtc.com>
2026-01-20 15:20:43 +0100

    create userFlow updated

--- src/api/authUser/userAuth.route.ts
+++ src/api/authUser/userAuth.route.ts
@@ -1,12 +1,14 @@
-import { Router } from "express";
+import {Router} from "express";
 import * as controller from "./userAuth.controller.js";
 import auth from "../middleware/auth.js";
+import {userLogin} from "./userAuth.controller.js";
 
 export default function registerUserAuthRoutes(app: Router) {
-  const router = Router();
+    const router = Router();
 
-  router.post("/login", auth);
-  router.post("/generate-otp", controller.generateOtp);
-  router.post("/validate-otp", controller.validateOtp);
-  app.use("/user-auth", router);
+    router.get("/login", controller.userLogin);
+    router.post("/generate-otp", controller.generateOtp);
+    router.post("/otp-for-registrant", controller.generateTokenForRegistrant);
+    router.post("/validate-otp", controller.validateOtp);
+    app.use("/user-auth", router);
 }
