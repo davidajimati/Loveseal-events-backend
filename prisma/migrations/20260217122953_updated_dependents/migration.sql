6cd474281b70f7c24b95937de361a58b43782f3a
David Ajimati <david.ajimati@stanbicibtc.com>
2026-03-01 10:04:23 +0100

    removed recent migration file

--- prisma/migrations/20260217122953_updated_dependents/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `registrant_dependants_table_paymentReference_key` ON `registrant_dependants_table`;
-
--- CreateIndex
-CREATE INDEX `registrant_dependants_table_paymentReference_idx` ON `registrant_dependants_table`(`paymentReference`);
